34b7af7d9431df6e5f83f0482e244e7877d88e3c
manya1606 <71758041+manya1606@users.noreply.github.com>
2021-05-06 16:26:11 +0530

    Add files via upload

---
+++ DustbinClass.js
@@ -0,0 +1,15 @@
+class Dustbin{
+    constructor(x,y,width,height){
+        
+        this.body = Bodies.rectangle(x,y,width,height)
+        this.width = width;
+        this.height = height;
+        World .add (world,this.body)
+     }
+     display(){
+         var pos = this.body.position 
+         rectMode (CENTER)
+         fill ("green")
+         rect (pos.x,pos.y,this.width,this.height)
+     }
+    }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project-24-2
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
